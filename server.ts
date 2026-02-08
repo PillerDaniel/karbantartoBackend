d6f028ce6e3d481bd1eb478da1785a6af5805df4
PillerDaniel <pdanitwitch@gmail.com>
2026-02-08 19:35:30 +0100

    initial commit

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import config from 'config';
+import cors from 'cors';
+
+const PORT = config.get<number>('PORT');
+const app: any = express();
+
+const corsOptions: cors.CorsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get('/api', (_req: Request, res: Response) => {
+    res.send('API running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
