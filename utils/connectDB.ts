b53cf2d079774debdc351cfefcba28355ec0d9f1
PillerDaniel <pdanitwitch@gmail.com>
2026-02-11 01:18:15 +0100

    First migration, connectDB changes

--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -1,19 +1,6 @@
-import { DataSource } from 'typeorm';
-import config from 'config';
-
-const DBURL = config.get<string>('DBURL');
+import AppDataSource from '../config/dataSource';
 
 const connectDB = async (): Promise<void> => {
-    const AppDataSource = new DataSource({
-        type: 'postgres',
-        url: DBURL,
-        synchronize: true,
-        logging: false,
-        entities: ['../models/**/*.ts'],
-        migrations: ['../migrations/**/*.ts'],
-        subscribers: ['../subscribers/**/*.ts'],
-    });
-
     try {
         await AppDataSource.initialize();
         console.log('Database connected successfully');
